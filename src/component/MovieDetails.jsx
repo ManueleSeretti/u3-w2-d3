b219a1035125d532275b2f5c20f637764f1a4183
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-09-13 17:35:19 +0200

    ...

--- src/component/MovieDetails.jsx
+++ src/component/MovieDetails.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Alert, Button, Card, Col, Container, ListGroup, Row } from "react-bootstrap";
+import { Alert, Button, Card, Col, Container, Form, ListGroup, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 const options = {
   headers: {
@@ -13,12 +13,13 @@ const MovieDetails = () => {
   const [details, setDetails] = useState({});
   const [comments, setComments] = useState([]);
   const [showComments, setShowComments] = useState(false);
+  const [comm, setComm] = useState({ comment: "", rate: "" });
 
   const DetailsFetch = async () => {
     try {
       const resp = await fetch("http://www.omdbapi.com/?apikey=7dd82326&i=" + idFilm);
       const data = await resp.json();
-      console.log(data);
+
       setDetails(data);
     } catch (error) {}
   };
@@ -35,10 +36,32 @@ const MovieDetails = () => {
       setComments(data);
     } catch (error) {}
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const resp = await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+        method: "POST",
+        body: JSON.stringify({ ...comm, elementId: idFilm }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NTgwZGMwMzRmZjAwMTQwM2Y0ZjMiLCJpYXQiOjE2OTQwODgzNTcsImV4cCI6MTY5NTI5Nzk1N30.vrJTCqAqY2RHrfENaHsCahiSPiXPbIGG0RZTWaBGmrQ",
+        },
+      });
+      if (resp.ok) {
+        setComm({ comment: "", rate: "" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    commentFetch();
+  };
+
   const handleDelete = async (event, id) => {
     event.preventDefault();
     try {
-      const resp = await fetch("https://striveschool-api.herokuapp.com/api/movies/" + id, {
+      const resp = await fetch("https://striveschool-api.herokuapp.com/api/comments/" + id, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
@@ -73,26 +96,62 @@ const MovieDetails = () => {
         </Col>{" "}
         <Col xs={6}>
           {showComments ? (
-            <ListGroup className="p-2 bg-secondary">
-              <h5 className="text-info">Lista dei commenti:</h5>
-              {comments.length === 0 && <Alert variant="info">Non ci sono commenti al momento</Alert>}
-              {comments.map((comm, index) => (
-                <ListGroup.Item className="text-start" key={index}>
-                  Autore : {comm.author} <br />
-                  Commento : {comm.comment} <br />
-                  Rate : {comm.rate}
-                  <Button
-                    className="ms-5"
-                    variant="danger"
-                    onClick={(event) => {
-                      handleDelete(event, comm._id);
+            <>
+              <ListGroup className="p-2 bg-secondary">
+                <h5 className="text-info">Lista dei commenti:</h5>
+                {comments.length === 0 && <Alert variant="info">Non ci sono commenti al momento</Alert>}
+                {comments.map((comm, index) => (
+                  <ListGroup.Item className="text-start" key={index}>
+                    Autore : {comm.author} <br />
+                    Commento : {comm.comment} <br />
+                    Rate : {comm.rate}
+                    <Button
+                      className="ms-5"
+                      variant="danger"
+                      onClick={(event) => {
+                        handleDelete(event, comm._id);
+                      }}
+                    >
+                      delete
+                    </Button>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+              <Form className="mt-2 border bg-dark p-2" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3">
+                  <Form.Label className="text-white">Aggiungi un Commento</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="commenta"
+                    required
+                    value={comm.comment}
+                    onChange={(e) => {
+                      setComm({ ...comm, comment: e.target.value });
+                    }}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Select
+                    aria-label="Default select example"
+                    required
+                    value={comm.rate}
+                    onChange={(e) => {
+                      setComm({ ...comm, rate: e.target.value });
                     }}
                   >
-                    delete
-                  </Button>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
+                    <option>Seleziona un voto da 1 a 5</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Form.Select>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Invia
+                </Button>
+              </Form>
+            </>
           ) : (
             <Alert variant="info">clicca per vedere i commenti</Alert>
           )}
